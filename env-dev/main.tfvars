d91c468e082738654b1ea40d910fdfa587875d3e
brajasekharreddy <raj479.mits@gmail.com>
2023-02-14 02:23:27 +0530

    creating multiple subnets in all availablity zones(minimum 2) to maintain high availability of servers.

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,5 +4,6 @@ env = "dev"
 vpc = {
   main = {
     cidr_block   = "10.0.0.0/16"
+    subnets_cidr = ["10.0.0.0/17", "10.0.128.0/17"]
   }
 }
--- main.tf
+++ main.tf
@@ -10,5 +10,6 @@ module "network" {
 
   for_each     = var.vpc
   cidr_block   = each.value.cidr_block
+  subnets_cidr = each.value.subnets_cidr
 }
 
