60410cb8c804aabf0efc5780d65fe85d4355768f
brajasekharreddy <raj479.mits@gmail.com>
2023-02-13 23:17:58 +0530

    VPC Module

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+#resource "null_resource" "test" {
+#  provisioner "local-exec" {
+#    command = "echo ${var.env}"
+#  }
+#}
+
+module "network" {
+  source         = "https://github.com/rajrgithub/10-tf-module-vpc.git"
 }
