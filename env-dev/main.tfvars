b514d7940a39c6692062b6585af77862fb3529c4
brajasekharreddy <raj479.mits@gmail.com>
2023-02-16 00:48:11 +0530

    for subnets modules

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,9 +4,20 @@ default_vpc_id = "vpc-01cb60de8c920e6a8"
 vpc = {
   main = {
     cidr_block   = "10.0.0.0/16"
- #   subnets_cidr = ["10.0.0.0/17", "10.0.128.0/17"]
-    public_subnets_cidr  = ["10.0.0.0/24", "10.0.1.0/24"]
-    private_subnets_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
-    availability_zones   = ["us-east-1a", "us-east-1b"]
+  }
+}
+
+subnets = {
+  public = {
+
+  }
+  web = {
+
+  }
+  app = {
+
+  }
+  db = {
+
   }
 }
--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-#resource "null_resource" "test" {
-#  provisioner "local-exec" {
-#    command = "echo ${var.env}"
-#  }
-#}
 
 module "network" {
   source         = "github.com/rajrgithub/10-tf-module-vpc"
@@ -11,9 +6,5 @@ module "network" {
 
   for_each     = var.vpc
   cidr_block   = each.value.cidr_block
- # subnets_cidr = each.value.subnets_cidr
-  public_subnets_cidr  = each.value.public_subnets_cidr
-  private_subnets_cidr = each.value.private_subnets_cidr
-  availability_zones   = each.value.availability_zones
 }
 
