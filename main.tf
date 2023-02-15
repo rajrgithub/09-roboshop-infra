b1d4820dcb1675cb467ab00a5e04268403e0da0d
brajasekharreddy <raj479.mits@gmail.com>
2023-02-16 02:32:50 +0530

    for subnets modules

--- main.tf
+++ main.tf
@@ -13,18 +13,14 @@ module "subnets" {
   env= var.env
   default_vpc_id = var.default_vpc_id
 
+  vpc_id = module.vpc.vpc_id
+
   for_each     = var.subnets
   cidr_block   = each.value.cidr_block
   availability_zone = each.value.availability_zone
   name = each.value.name
-  vpc_id =  lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-
 }
 
-#output "vpc_id" {
-#  value = "module.vpc"
-#}
-
 output "vpc_id" {
-  value = lookup(module.vpc, each.value.vpc_name, null)
+  value = "module.vpc"
 }
