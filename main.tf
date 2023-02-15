844abd9b9d4bb22ea4201456f69f54dc75b5d0bb
brajasekharreddy <raj479.mits@gmail.com>
2023-02-16 02:27:37 +0530

    for subnets modules

--- main.tf
+++ main.tf
@@ -8,18 +8,23 @@ module "vpc" {
   cidr_block   = each.value.cidr_block
 }
 
-#module "subnets" {
-#  source         = "github.com/rajrgithub/11-tf-module-subnets"
-#  env= var.env
-#  default_vpc_id = var.default_vpc_id
-#  vpc_id = module.vpc.vpc_id
-#
-#  for_each     = var.subnets
-#  cidr_block   = each.value.cidr_block
-#  availability_zone = each.value.availability_zone
-#  name = each.value.name
+module "subnets" {
+  source         = "github.com/rajrgithub/11-tf-module-subnets"
+  env= var.env
+  default_vpc_id = var.default_vpc_id
+
+  for_each     = var.subnets
+  cidr_block   = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name = each.value.name
+  vpc_id =  lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+
+}
+
+#output "vpc_id" {
+#  value = "module.vpc"
 #}
 
 output "vpc_id" {
-  value = "module.vpc"
+  value = lookup(module.vpc, each.value.vpc_name, null)
 }
