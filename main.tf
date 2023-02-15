547cc2e8bb065cda7dcfea285c2521ffe633177c
brajasekharreddy <raj479.mits@gmail.com>
2023-02-16 01:47:04 +0530

    for subnets modules

--- main.tf
+++ main.tf
@@ -8,14 +8,18 @@ module "vpc" {
   cidr_block   = each.value.cidr_block
 }
 
-module "subnets" {
-  source         = "github.com/rajrgithub/11-tf-module-subnets"
-  env= var.env
-  default_vpc_id = var.default_vpc_id
-  vpc_id = module.vpc.vpc_id
+#module "subnets" {
+#  source         = "github.com/rajrgithub/11-tf-module-subnets"
+#  env= var.env
+#  default_vpc_id = var.default_vpc_id
+#  vpc_id = module.vpc.vpc_id
+#
+#  for_each     = var.subnets
+#  cidr_block   = each.value.cidr_block
+#  availability_zone = each.value.availability_zone
+#  name = each.value.name
+#}
 
-  for_each     = var.subnets
-  cidr_block   = each.value.cidr_block
-  availability_zone = each.value.availability_zone
-  name = each.value.name
+output "vpc_id" {
+  value = "module.vpc"
 }
