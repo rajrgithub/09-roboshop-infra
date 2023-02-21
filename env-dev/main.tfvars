e26165ebb20d8bd2da33fb74d1cedfd538af7baf
brajasekharreddy <raj479.mits@gmail.com>
2023-02-21 22:51:53 +0530

    subnets module inside vpc only

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,39 +1,35 @@
-env = "dev"
+
+env            = "dev"
 default_vpc_id = "vpc-01cb60de8c920e6a8"
 
 vpc = {
   main = {
-    cidr_block   = "10.0.0.0/16"
-  }
-}
+    cidr_block        = "10.0.0.0/16"
+    availability_zone = ["us-east-1a", "us-east-1b"]
+    public_subnets = {
+      public = {
+        name        = "public"
+        cidr_block  = ["10.0.0.0/24", "10.0.1.0/24"]
+        internet_gw = true
+      }
+    }
+    private_subnets = {
+      web = {
+        name       = "web"
+        cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
+        nat_gw     = true
+      }
+      app = {
+        name       = "app"
+        cidr_block = ["10.0.4.0/24", "10.0.5.0/24"]
+        nat_gw     = true
+      }
+      db = {
+        name       = "db"
+        cidr_block = ["10.0.6.0/24", "10.0.7.0/24"]
+        nat_gw     = true
+      }
+    }
 
-subnets = {
-  public = {
-    name              = "public"
-    vpc_name          = "main"
-    cidr_block        = ["10.0.0.0/24", "10.0.1.0/24"]
-    availability_zone = ["us-east-1a", "us-east-1b"]
-    internet_gw       = true
-  }
-  web = {
-    name              = "web"
-    vpc_name          = "main"
-    cidr_block        = ["10.0.2.0/24", "10.0.3.0/24"]
-    availability_zone = ["us-east-1a", "us-east-1b"]
-    nat_gw            = true
-  }
-  app = {
-    name              = "app"
-    vpc_name          = "main"
-    cidr_block        = ["10.0.4.0/24", "10.0.5.0/24"]
-    availability_zone = ["us-east-1a", "us-east-1b"]
-    nat_gw            = true
-  }
-  db = {
-    name              = "db"
-    vpc_name          = "main"
-    cidr_block        = ["10.0.6.0/24", "10.0.7.0/24"]
-    availability_zone = ["us-east-1a", "us-east-1b"]
-    nat_gw            = true
   }
 }
--- main.tf
+++ main.tf
@@ -1,33 +1,32 @@
-
 module "vpc" {
   source         = "github.com/rajrgithub/10-tf-module-vpc"
-  env= var.env
+  env            = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each     = var.vpc
-  cidr_block   = each.value.cidr_block
-}
-
-module "subnets" {
-  source         = "github.com/rajrgithub/11-tf-module-subnets"
-  env= var.env
-  default_vpc_id = var.default_vpc_id
-
-  for_each     = var.subnets
-  cidr_block   = each.value.cidr_block
+  for_each          = var.vpc
+  cidr_block        = each.value.cidr_block
+  public_subnets    = each.value.public_subnets
+  private_subnets   = each.value.private_subnets
   availability_zone = each.value.availability_zone
-  name = each.value.name
-  vpc_id =  lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-  internet_gw_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "internet_gw_id", null)
-  internet_gw               = lookup(each.value, "internet_gw", false)
-  nat_gw                    = lookup(each.value, "nat_gw", false)
 }
 
-#output "vpc_id" {
-#  value = "module.vpc"
-#}
+//module "subnets" {
+//  source         = "github.com/raghudevopsb70/tf-module-subnets"
+//  env            = var.env
+//  default_vpc_id = var.default_vpc_id
+//
+//  for_each                  = var.subnets
+//  cidr_block                = each.value.cidr_block
+//  availability_zone         = each.value.availability_zone
+//  name                      = each.value.name
+//  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+//  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
+//  internet_gw               = lookup(each.value, "internet_gw", false)
+//  nat_gw                    = lookup(each.value, "nat_gw", false)
+//}
 
-#output "vpc_id" {
-#  value = lookup(module.vpc, each.value.vpc_name, null)
-#}
+//
+output "out" {
+  value = module.vpc
+}
+
